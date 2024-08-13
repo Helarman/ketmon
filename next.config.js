6464fa5591efcc06a5e71ddf8d94af48e2137d93
Helarman <helarman@bk.ru>
2024-08-14 00:56:50 +0300

    config

---
+++ next.config.js
@@ -0,0 +1,10 @@
+// @ts-check
+ 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['31.128.45.168:1337'],
+      },
+  }
+   
+  module.exports = nextConfig
